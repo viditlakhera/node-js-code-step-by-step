966326aa21ac9b2a31b9281e46219cdcc4bf69f5
vidit <lakheravidit@gmail.com>
2023-02-28 12:10:26 +0530

    process.argv use for file filed add and remove

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const http = require('http');
-http.createServer((req,res)=>{
-res.write(JSON.stringify({data:"hello welcome to node js course again"}));
-res.end();
-}).listen(4500);
+const fs = require("fs");
+const input = process.argv;
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("invalid input");
+}
---
+++ user.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        name:"vidit",
+        age:"22"
+    },
+    {
+        name:"sumit",
+        age:"23"
+    },
+    {
+        name:"kripal",
+        age:"21"
+    }
+]
+
+module.exports = users;
+
+
+
