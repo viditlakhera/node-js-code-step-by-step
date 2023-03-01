42b9e29edb9b9c727123f95d2289ef9ab0b1e1e7
vidit <lakheravidit@gmail.com>
2023-03-01 15:16:02 +0530

    async js using promises and async await

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const http = require('http');
-http.createServer((req,res)=>{
-res.write(JSON.stringify({data:"hello welcome to node js course again"}));
-res.end();
-}).listen(4500);
+// let a = 20;
+// let b = 30;
+// setTimeout(()=>{
+//    b=24;
+// },2000);
+// console.log(a+b);          // if some vlaue is updated in asynch js how can we handle
+
+//now concept of callbacks,promises,async await comes
+
+let a = 20;
+let b = 30;
+let waitingData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(24);
+  }, 2000);
+});
+
+// waitingData.then(()=>{console.log(a+b)});  //so what happen here is they wait until resolve value executed
+// we can do any task inside resolve ()
+// after that its value stored in waitingData
+//.then executed()
+
+const addition = async () => {
+  await setTimeout(() => {
+    b = 24;
+  }, 2000);
+
+  console.log(a + b); // using async await we can do same things as in promises
+};
+
+addition();
